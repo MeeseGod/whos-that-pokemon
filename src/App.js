3b02bbad001a1a2cdb96eb7493cb5ee75442a64a
JosephR <joseph_rundell@outlook.com>
2022-04-29 14:46:16 -0400

    Added PokemonDisplay component, successfully attached firebase config to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import {db} from "./firebase"
+import {collection, getDocs} from "firebase/firestore"
+import { useEffect } from "react"
+
+import PokemonDisplay from "../src/component/PokemonDisplay";
+
+
 function App() {
+
+  const pokemonCollection = collection(db, "pokemon")
+
+  useEffect(() => {
+    const getPokemon = async () => {
+      const pokemon = await getDocs(pokemonCollection);
+      console.log(pokemon.docs[0].data())
+    }
+    getPokemon();
+  }, [])
+
+  function getClick(event){
+    console.log(event.screenX, event.screenY)
+  }
+
   return (
-    <div className="App">
+    <div className="App" onClick={(e) => getClick(e)}>
       Pokemon Project
+      <PokemonDisplay/>
     </div>
   );
 }
---
+++ src/component/PokemonDisplay.js
@@ -0,0 +1,9 @@
+export default function PokemonDisplay(){
+    return (
+        <div className="pokemonDisplayContainer">
+            <div className="mainImageContainer">
+                <img className="pokemonImage" src={require("../images/pokemonCollage.jpg")}/>
+            </div>
+        </div>
+    )
+}
